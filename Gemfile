1bde7d7cd0e0c01b915b88503ee6dcb576b1b20b
Bay Gross <bgross17@gmail.com>
2013-06-21 15:16:06 -0700

    huh, deploy is just breaking

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'http://rubygems.org'
 gem 'sinatra'
 gem 'sass'
+require 'newrelic_rpm'
--- app.rb
+++ app.rb
@@ -6,5 +6,9 @@ require 'bundler/setup'
 require 'sinatra'
 require 'sass'
 
+configure :production do
+  require 'newrelic_rpm'
+end
+
 #include routes
 require File.join(File.dirname(__FILE__), 'routes')
